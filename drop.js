81f1163d056a3e470c3ec45fa7acfbf88f22499c
raagavan7 <76561156+raagavan7@users.noreply.github.com>
2021-06-14 20:38:22 +0530

    Add files via upload

---
+++ drop.js
@@ -0,0 +1,9 @@
+class DROPS{
+    constructor(x,y){
+       var options={
+        restitution:0.3,
+        friction:0.1,
+        density:1.2}
+
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,17 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+function preload(){
+    
+}
+
+function setup(){
+   createCanvas(500,800)
+    
+}
+
+function draw(){
+    background("black")
+}   
+
